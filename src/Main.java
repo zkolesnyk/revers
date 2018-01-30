e1732bc4d1db2ba2f199efb83063d98fe6667e50
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-01-30 20:27:07 +0200

    лишняя переменная

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String string = "java asd qwe 123 123 1asd asd";
         char[] chars = new char[string.length()];
-        String newString = "";
         for (int i = 0; i < string.length(); i++) {
             chars[i] = string.charAt(string.length() - i - 1);
         }
