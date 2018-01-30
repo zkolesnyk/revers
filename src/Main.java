e7117eeb1513b8be298ccb6410afdbc65e55cd95
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-01-30 20:39:24 +0200

    лишний массив

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String string = "java asd qwe 123 123 1asd asd";
-        char[] chars = new char[string.length()];
         for (int i = 0; i < string.length(); i++) {
-            chars[i] = string.charAt(string.length() - i - 1);
-            System.out.print(chars[i]);
+            System.out.print(string.charAt(string.length() - i - 1));
         }
-
     }
 }
