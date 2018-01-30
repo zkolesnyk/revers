cf2d919ecbe0190645bf8dc4ca9dbade831667ad
Yevhenii Kolesnyk <z_kolesnyk@ukr.net>
2018-01-30 20:14:43 +0200

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String string = "java";
+        String string = "java asd qwe 123 123 1asd asd";
         char[] chars = new char[string.length()];
         String newString = "";
         for (int i = 0; i < string.length(); i++) {
             chars[i] = string.charAt(i);
         }
 
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = string.length()-1; i >= 0; --i) {
             System.out.print(chars[i]);
         }
     }
-
 }
